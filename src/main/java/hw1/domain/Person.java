8137ccef7dea527cc07cc5981c05493535e2a5eb
ЕГОР <egor@MacBook.local>
2024-04-04 12:48:28 +0300

    Homework1 added

---
+++ src/main/java/hw1/domain/Person.java
@@ -0,0 +1,81 @@
+package hw1.domain;
+
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Person {
+    @SerializedName("name")
+    private String firstName; // имя
+    @SerializedName("surname")
+    private String lastName; // фамилия
+
+    private int age; // возраст
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // переопределенный метод преобразования в строку
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("name", firstName)
+                .append("surname", lastName)
+                .append("age", age)
+                .toString();
+    }
+
+    // переопределенный метод сравнения
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return new EqualsBuilder()
+                .append(age, person.age)
+                .append(firstName, person.firstName)
+                .append(lastName, person.lastName)
+                .isEquals();
+    }
+
+    // переопределенный метод генерации хэш кода
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(firstName)
+                .append(lastName)
+                .append(age)
+                .toHashCode();
+    }
+}
